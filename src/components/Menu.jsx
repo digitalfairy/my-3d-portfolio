ca94ee0c1093f02262977440ac7ac15c09aca9e1
Anita <jelicanita7@gmail.com>
2026-03-04 20:06:30 +0100

    Menu.jsx button
    Adjusting the inner lines.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,11 +6,11 @@ export const Menu = (props) => {
       {/* Menu Trigger Button */}
       <button
         onClick={() => setMenuOpened(!menuOpened)}
-        className="z-50 fixed top-6 right-6 md:top-12 md:right-12 p-3 bg-indigo-950 border-indigo-900/50 w-8 h-8 md:w-12 md:h-12 rounded-xl backdrop-blur-sm flex flex-col justify-center items-center gap-1.5 transition-all hover:bg-slate-800"
+        className="z-50 fixed top-4 right-3 md:top-12 md:right-12 p-2 bg-indigo-950 border border-indigo-900/50 w-8 h-8 md:w-12 md:h-12 rounded-xl backdrop-blur-md flex flex-col justify-center items-center gap-1 transition-all hover:bg-slate-800"
       >
-        <div className={`bg-white h-0.5 rounded-full w-4 md:w-6 transition-all ${menuOpened ? "rotate-45 translate-y-2" : ""}`} />
+        <div className={`bg-white h-0.5 rounded-full w-4 md:w-6 transition-all ${menuOpened ? "rotate-45 translate-y-[5px]" : ""}`} />
         <div className={`bg-white h-0.5 rounded-full w-4 md:w-6 transition-all ${menuOpened ? "opacity-0" : ""}`} />
-        <div className={`bg-white h-0.5 rounded-full w-4 md:w-6 transition-all ${menuOpened ? "-rotate-45 -translate-y-2" : ""}`} />
+        <div className={`bg-white h-0.5 rounded-full w-4 md:w-6 transition-all ${menuOpened ? "-rotate-45 -translate-y-[5px]" : ""}`} />
       </button>
 
       {/* Menu Overlay */}
