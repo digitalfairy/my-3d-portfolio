50d6ba594ee1e089bdc12488a4c8eaae883e3765
Anita <jelicanita7@gmail.com>
2026-03-04 16:31:51 +0100

    Background.jsx update

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,30 +3,39 @@ import { useFrame } from "@react-three/fiber";
 import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+
 export const Background = () => {
   const material = useRef();
   const color = useRef({
-    color: "#b9bcff",
+    color: "#9b96dd", 
   });
   const data = useScroll();
-
   const tl = useRef();
 
   useFrame(() => {
-    tl.current.progress(data.scroll.current);
-    material.current.color = new THREE.Color(color.current.color);
+    if (tl.current) {
+      tl.current.progress(data.scroll.current);
+      material.current.color = new THREE.Color(color.current.color);
+    }
   });
 
   useEffect(() => {
     tl.current = gsap.timeline();
+    
     tl.current.to(color.current, {
-      color: "#212121",
+      color: "#9b96dd", 
     });
     tl.current.to(color.current, {
-      color: "#7a7ca5",
+      color: "#212121", 
     });
     tl.current.to(color.current, {
-      color: "#9b96dd",
+      color: "#9b96dd", 
+    });
+     tl.current.to(color.current, {
+      color: "#212121", 
+    });
+    tl.current.to(color.current, {
+      color: "#9b96dd", 
     });
   }, []);
 
